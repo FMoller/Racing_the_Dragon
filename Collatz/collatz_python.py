7972e8b17edfa29b83d12c1fe0de6205fee8b81f
FMoller <fredericomollerper@gmail.com>
2021-07-26 12:30:31 -0300

    benchmark added

--- Collatz/collatz_python.py
+++ Collatz/collatz_python.py
@@ -5,6 +5,9 @@ https://github.com/FMoller/Racing_the_Dragon
 
 __author__ = 'Frederico Moeller'
 
+import time
+import numpy as np
+
 def r_collatz(n,c_list):
     c_list.append(n)
     if n>1:
@@ -40,8 +43,26 @@ def i_collatz2(n,c_list):
         n=n/2
     c_list.append(n)
 
+test_val = np.array([744, 340, 480, 679, 227, 996, 984, 753, 931, 679, 325, 939, 153,
+       529, 535, 914, 804, 746, 453,  58, 668, 406, 217, 254, 830, 530,
+       867, 983, 149, 306, 852, 398, 229, 736,   9, 406, 184, 377, 322,
+       367, 960, 583, 355, 534, 980, 906, 474, 921, 895, 504, 586, 281,
+        32, 371, 866, 172, 478, 906, 844, 140])
+
+rcltz = []
+rcltz2 = []
+icltz = []
+icltz2 = []
+
+for i in test_val:
+    c_list = []
+    st_time = time.perf_counter()
+    rcltz.append(r_collatz(i,c_list))
+    ed_time = time.perf_counter()
+    rcltz.append(ed_time - st_time)
     
-        
+    
+    
         
 
     
